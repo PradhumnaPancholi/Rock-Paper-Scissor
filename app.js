421eef177b35caf7246d95526a2b46e0b11786c1
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-05-10 18:56:07 -0400

    minor refactors, got logic working, Note: need to work on UI

--- app.js
+++ app.js
@@ -3,13 +3,56 @@ const rockEle = document.getElementById("rock")
 const scissorEle = document.getElementById("scissor")
 const paperEle = document.getElementById("paper")
 
-const choices = ["rock", "scissor", "paper"]
+const choices = ["r", "s", "p"]
+let compInput = ""
+let userInput = ""
 
 //logic to randomly selection (computer side)//
-var computerSelection = () => {
+var computersSelection = () => {
     var selection = choices[Math.floor(Math.random() * choices.length)]
-    console.log(selection)
+    compInput = selection
 
+}
+
+//function for user's input choice//
+rockEle.addEventListener("click", function(event) {
+    userInput = event.target.value
+    getResult()
+})
+
+scissorEle.addEventListener("click", function(event) {
+    userInput = event.target.value
+    getResult()
+})
+
+paperEle.addEventListener("click", function(event) {
+    userInput = event.target.value
+    getResult()
+})
+
+//function to compare inputs and get result//
+var getResult = () => {
+    computersSelection()
+    console.log(compInput, userInput)
+    
+    //Switch to compare input and declare result//
+    switch(userInput+compInput){
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("You won")
+            break;
+        case "rr":
+        case "ss":
+        case "pp":
+            console.log("It's an Tie")
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log("You lose")
+            break;
+    }
 }
 
 
